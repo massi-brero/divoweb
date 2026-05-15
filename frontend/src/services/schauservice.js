4878070028232378185d6854fa628fb96adacb16
jmba <jm.bayerl@googlemail.com>
2026-05-15 22:07:19 +0200

    Render-URL

--- frontend/src/services/schauservice.js
+++ frontend/src/services/schauservice.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:8080/api/v1";
+//const API_BASE_URL = "http://localhost:8080/api/v1";
+const API_BASE_URL = "https://wellensittich.onrender.com/api/v1";
 
 /**
  * @typedef {Object} SchauAnlegenRequest
