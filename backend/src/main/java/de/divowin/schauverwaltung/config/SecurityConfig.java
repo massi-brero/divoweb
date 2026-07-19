a63ccce71d61eebacabe5b92ba6a7a7db1f0cd28
jmba <jm.bayerl@googlemail.com>
2026-07-19 13:55:52 +0200

    Cors-Regel hinzugefügt

--- backend/src/main/java/de/divowin/schauverwaltung/config/SecurityConfig.java
+++ backend/src/main/java/de/divowin/schauverwaltung/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
-                "https://frontend-lsv0.onrender.com"
+                "https://wellensittich.jbayerl.de"
         ));
 
         config.setAllowedMethods(List.of(
